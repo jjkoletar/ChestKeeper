00ba2363d254604eeb3071522b9b9d7cbcf35542
Jeremy Koletar <jjkoletar@gmail.com>
2013-01-06 22:29:32 -0600

    Wtf...

--- src/com/koletar/jj/chestkeeper/Phrases.java
+++ src/com/koletar/jj/chestkeeper/Phrases.java
@@ -1,7 +1,64 @@
 package com.koletar.jj.chestkeeper;
 
+import org.bukkit.World;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
 /**
  * @author jjkoletar
  */
 public class Phrases {
+    private static Phrases instance;
+    private ResourceBundle phrases;
+
+    private Phrases() {}
+
+    public static Phrases getInstance() {
+        if (instance == null) {
+            instance = new Phrases();
+        }
+        return instance;
+    }
+
+    public void initialize(Locale l) {
+        phrases = ResourceBundle.getBundle("phrases", l);
+    }
+
+    public static String phrase(String key, Object... replacements) {
+        if (getInstance() == null) {
+            return "";
+        }
+        if (getInstance().phrases == null) {
+            return "\u00A74Phrase Error! Did you /reload? Don't!";
+        }
+        if (!getInstance().phrases.containsKey(key)) {
+            Logger.getLogger("Minecraft").warning("[ChestKeeper] Unknown phrase key! '" + key + "'");
+            return "";
+        }
+        String format = getInstance().phrases.getString(key);
+        for (int i = 0; i < replacements.length; i++) {
+            format = format.replace("%" + i + "%", findName(replacements[i]));
+        }
+        format = format.replace("&", "\u00A7").replace("\u00A7\u00A7", "&");
+        return format;
+    }
+
+    public static String findName(Object o) {
+        if (o instanceof Player) {
+            return ((Player) o).getName();
+        } else if (o instanceof World) {
+            return ((World) o).getName();
+        } else if (o instanceof ConsoleCommandSender) {
+            return phrase("console");
+        } else if (o instanceof CKUser) {
+            return ((CKUser) o).getUsername();
+        } else if (o instanceof CKChest) {
+            return ((CKChest) o).getTitle();
+        }
+        return o.toString();
+    }
 }
--- src/com/koletar/jj/chestkeeper/StringTools.java
+++ src/com/koletar/jj/chestkeeper/StringTools.java
@@ -1,7 +1,24 @@
 package com.koletar.jj.chestkeeper;
 
+import java.util.Collection;
+
 /**
  * @author jjkoletar
  */
 public class StringTools {
+    public static String buildList(Object[] items, String prefix, String suffix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < items.length; i++) {
+            sb.append(prefix);
+            sb.append(Phrases.findName(items[i]));
+            if (i < items.length - 1) {
+                sb.append(suffix);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String buildList(Collection<?> items, String prefix, String suffix) {
+        return buildList(items.toArray(), prefix, suffix);
+    }
 }
