58e094f571127e76f9931318fa2117010e0af1ec
Jeremy Koletar <jjkoletar@gmail.com>
2013-01-07 19:18:15 -0600

    Fix special character chest names converting funkily

--- src/com/koletar/jj/chestkeeper/CKUser.java
+++ src/com/koletar/jj/chestkeeper/CKUser.java
@@ -180,7 +180,7 @@ public class CKUser implements ConfigurationSerializable {
                             chests.put(currentChest.toLowerCase(), chest);
                             items.clear();
                         }
-                        currentChest = line.substring(0, line.length() - 1);
+                        currentChest = line.substring(0, line.length() - 1).replace("'", "");
                     } else if (line.startsWith("  type: ")) {
                         isLargeChest = line.contains("large");
                     } else if (line.startsWith("  eitems:")) {
